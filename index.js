6f7e4fda2178da79c318cadc21b61b3e14ef6d14
aidanmaskell <aidan.maskell@gmail.com>
2022-10-06 08:59:37 +0100

    slwoed down animations

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 gsap.registerPlugin('ScrollTrigger');
 
 gsap.to(".am", { // selector text, Array, or object
-    x: 200, // any properties (not limited to CSS)
+    x: 50, // any properties (not limited to CSS)
     opacity: 1,
     duration: 1.5, // seconds
 });
 
 gsap.to(".welcome", { // selector text, Array, or object
-    x: -200, // any properties (not limited to CSS)
+    x: -50, // any properties (not limited to CSS)
     opacity: 1,
     duration: 1.5, // seconds
 });
@@ -19,42 +19,46 @@ gsap.to(".nava", { // selector text, Array, or object
 });
 
 gsap.to(".me", { // selector text, Array, or object
-    x: -200, // any properties (not limited to CSS)
+    x: -50, // any properties (not limited to CSS)
     opacity: 1,
     duration: 1, 
     scrollTrigger: {
-        trigger: ".me"
+        trigger: ".me",
+        start: "top center",  // [trigger] [scroller] positions
     }
 });
 
 gsap.to(".about", { // selector text, Array, or object
-    x: 200, // any properties (not limited to CSS)
+    x: 50, // any properties (not limited to CSS)
     opacity: 1,
     duration: 1, 
     scrollTrigger: {
-        trigger: ".about"
+        trigger: ".about",
+        start: "top center",  // [trigger] [scroller] positions
     }
 });
 
 
 document.querySelectorAll('.proj-top').forEach(projTop => {
     gsap.to(projTop, { // selector text, Array, or object
-        x: -100, // any properties (not limited to CSS)
+        x: -50, // any properties (not limited to CSS)
         opacity: 1,
         duration: 1, 
         scrollTrigger: {
-            trigger: projTop
+            trigger: projTop,
+            start: "top center",  // [trigger] [scroller] positions
         }
     })
 })
 
 document.querySelectorAll('.proj-bottom').forEach(projBot => {
     gsap.to(projBot, { // selector text, Array, or object
-        x: 100, // any properties (not limited to CSS)
+        x: 50, // any properties (not limited to CSS)
         opacity: 1,
         duration: 1, 
         scrollTrigger: {
-            trigger: projBot
+            trigger: projBot,
+            start: "top center",  // [trigger] [scroller] positions
         }
     })
 })
